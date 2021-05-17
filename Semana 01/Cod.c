0925a44d46c8fc88b58f13049129d3c6e8e6d692
FranciscoGJR <franciscoogjr@gmail.com>
2021-05-17 11:18:12 -0300

    Initial commit

---
+++ Semana 01/Cod.c
@@ -0,0 +1,6 @@
+Teste de execucao de um primeiro programa.
+Este programa imprime coisas na tela.
+Em quantas linhas diferentes?
+Quatro?
+Cinco?
+Seis?
---
+++ Semana 03/codigo.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    int meuNumeroUSP = 12683190;
+    printf("meuNumeroUSP\n");
+    printf("%i\n",meuNumeroUSP);
+    printf("%i\n",meuNumeroUSP / 5);
+    printf("%i\n",meuNumeroUSP % 5);
+    return 0;
+}
