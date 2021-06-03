5697c3b7542d3e2abce87671e433d1fca77e0755
FranciscoGJR <franciscoogjr@gmail.com>
2021-06-03 12:21:06 -0300

    Add exceções

--- EPs/EP 2/Cod teste.c
+++ EPs/EP 2/Cod teste.c
@@ -4,7 +4,9 @@ double modulo(double number){
 }
 
 double raizQuadrada(double a, double epsilon) {
-	double resposta = 0.42;
+    if(a < 0 || epsilon <= 0 || epsilon >= 1) return -1;
+    if(a == 0) return 0;
+
     double xo, xi;
 
     xo = a/2;
@@ -13,9 +15,11 @@ double raizQuadrada(double a, double epsilon) {
 	xi = (a/xo + xo)/2;
 
 
-    printf("x0 = %f, x1 = %f\n", xo, xi);
+//    printf("x0 = %f, x1 = %f\n", xo, xi);
 
-	if (modulo(xi - xo) <= epsilon) return resposta;
+
+	if (modulo(xi - xo) <= epsilon) {
+        return xi;}
 
 	xo = xi;
     }
@@ -25,9 +29,19 @@ double raizQuadrada(double a, double epsilon) {
 int main() {
 
 
-	double valor = 63;
+	double valor = 16;
 	double precisao = 0.001;
-	raizQuadrada(valor, precisao);
 
+	printf("(35) (0.00342) = %f\n", raizQuadrada(35, 0.00342));
+
+	printf("(45674) (0.042) = %f\n", raizQuadrada(45674, 0.042));
+
+	printf("(-4) (0.042) = %f\n", raizQuadrada(-4, 0.042));
+
+	printf("(0) (0.042) = %f\n", raizQuadrada(0, 0.042));
+
+	printf("(42) (6) = %f\n", raizQuadrada(42, 6));
+
+	printf("(42) (-87) = %f\n", raizQuadrada(42, -87));
 	return 0;
 }
