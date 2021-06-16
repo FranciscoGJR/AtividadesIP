06aa9596016a364c3d6aa0a633aa06f6da441737
FranciscoGJR <franciscoogjr@gmail.com>
2021-06-16 09:21:41 -0300

    Testes de entradas

--- EPs/EP 2/12683190.c
+++ EPs/EP 2/12683190.c
@@ -43,20 +43,11 @@ double raizQuadrada(double a, double epsilon) {
 
 int main() {
         //TESTES
-	printf("(35) (0.00342) = %f\n", raizQuadrada(35, 0.00342));
-
-	printf("(45674) (0.042) = %f\n", raizQuadrada(45674, 0.042));
-
-	printf("(0) (0.042) = %f\n", raizQuadrada(0, 0.042));
-
-	printf("(-4) (0.042) = %f\n", raizQuadrada(-4, 0.042));
-
-	printf("(91) (0) = %f\n", raizQuadrada(91, 0));
-
-	printf("(91) (1) = %f\n", raizQuadrada(92, 1));
-
-	printf("(42) (6) = %f\n", raizQuadrada(42, 6));
-
-	printf("(42) (-87) = %f\n", raizQuadrada(42, -87));
+printf("(64) (0.0001) = %f\n", raizQuadrada(64, 0.0001));
+	printf("(25) (0.0001) = %f\n", raizQuadrada(25, 0.0001));
+	printf("(124) (0.042) = %f\n", raizQuadrada(-4, 0.042));
+	printf("(0) (-1) = %f\n", raizQuadrada(0, -1));
+	printf("(626) (6) = %f\n", raizQuadrada(626, 6));
+	printf("(34) (0.0001) = %f\n", raizQuadrada(34, 0.0001));
 	return 0;
 }
